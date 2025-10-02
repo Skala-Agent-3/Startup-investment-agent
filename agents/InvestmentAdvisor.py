886faa769dc5a4a15f3cb61bad8a4927e85e26ba
im-hyemm <geohyemm9962@gmail.com>
2025-10-02 14:58:54 +0900

    [feat] add InvestmentAdvisor agent and prompts

---
+++ agents/InvestmentAdvisor.py
@@ -0,0 +1,44 @@
+﻿from graph_state import GraphState
+
+from dotenv import load_dotenv
+from langchain_teddynote import logging
+from typing import Annotated, Sequence, TypedDict, List
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from pathlib import Path
+from prompts.investment_advisor import INVESTMENT_ADVISOR_USER_TEMPLATE, INVESTMENT_ADVISOR_SYSTEM_TEMPLATE
+
+import logging
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+load_dotenv()
+
+def InvestmentAdvisor(state):
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    parser = StrOutputParser()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", INVESTMENT_ADVISOR_SYSTEM_TEMPLATE),
+            ("human", INVESTMENT_ADVISOR_USER_TEMPLATE)
+        ],
+        template_format="jinja2"
+    )
+
+    chain = prompt | llm | parser
+
+    response = chain.invoke({
+        "competitor": state.get("CompetitorAnalyzer", ""),
+        "market_eval": state.get("MarketEvaluator", "")
+    })
+
+
+    report = response.content if hasattr(response, "content") else str(response)
+    print("InvestmentAdvisor state snapshot")
+    print(state)
+    return {
+        "InvestmentAdvisor": report
+    }
+
+
